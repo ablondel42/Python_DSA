49b948ef8ac4c484a67732e4483147ea75b46890
arnaudblondel <ablondel42@icloud.com>
2026-02-27 09:48:23 +0100

    hash_table using list

---
+++ hash_table/hash_table.py
@@ -0,0 +1,53 @@
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    
+    def __hash(self, key):
+        hash = 0
+        for letter in key:
+            hash = (hash + ord(letter) * 23) % len(self.data_map)
+        return hash
+    
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+    
+
+    def get_keys(self):
+        keys = []
+
+        for index in range(len(self.data_map)):
+            if self.data_map[index] is not None:
+                for i in range(len(self.data_map[index])):
+                    if self.data_map[index][i] is not None:
+                        keys.append(self.data_map[index][i][0])
+        return keys
+
+
+hash_table = HashTable()
+
+hash_table.set_item("bolts", 1400)
+hash_table.set_item("washers", 50)
+hash_table.set_item("lumber", 70)
+hash_table.print_table()
+
+print(hash_table.get_item("washers"))
+print(hash_table.get_keys())
