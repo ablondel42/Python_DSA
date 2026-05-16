9cc5b539b23c337148a2d7c934ed33f09f1f9f76
ablondel42 <ablondel42@icloud.com>
2026-05-16 20:31:38 +0200

    Implement binary search algorithm in BinarySearchList.py

--- _fm_algo_course/python_machine/src/day1/BinarySearchList.py
+++ _fm_algo_course/python_machine/src/day1/BinarySearchList.py
@@ -1,4 +1,17 @@
 from typing import List
 
 def binary_search(haystack: List[int], needle: int) -> bool:
-    pass
+    low, high = 0, len(haystack)
+
+    while low < high:
+        mid = low + (high - low) // 2
+        val = haystack[mid]
+        
+        if val > needle:
+            high = mid
+        elif val < needle:
+            low = mid + 1
+        else:
+            return True
+        
+    return False
